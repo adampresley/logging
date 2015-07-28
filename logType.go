47bfa9469b1b05e55f2a45373d1e1c7e2792024b
Adam Presley <adam@adampresley.com>
2015-07-27 20:13:53 -0500

    Added log levels

--- logType.go
+++ logType.go
@@ -1,9 +1,18 @@
 package logging
 
+import "strings"
+
+/*
+LogType represents a type and level of logging
+*/
 type LogType int
 
+/*
+Constants for the type and levels of logging
+*/
 const (
 	NONE LogType = iota
+	DEBUG
 	INFO
 	WARN
 	ERROR
@@ -12,12 +21,30 @@ const (
 
 var logTypeNames = map[LogType]string{
 	NONE:  "None",
+	DEBUG: "DEBUG",
 	INFO:  "INFO",
 	WARN:  "WARNING",
 	ERROR: "ERROR",
 	FATAL: "FATAL",
 }
 
-func (this LogType) String() string {
-	return logTypeNames[this]
+/*
+String returns the friendly name of a specified log type/level
+*/
+func (logType LogType) String() string {
+	return logTypeNames[logType]
+}
+
+/*
+StringToLogType converts a specified string to a LogType. If the string does not
+match a specific log type the NONE is returned.
+*/
+func StringToLogType(logTypeName string) LogType {
+	for logType, stringValue := range logTypeNames {
+		if strings.ToLower(stringValue) == strings.ToLower(logTypeName) {
+			return logType
+		}
+	}
+
+	return NONE
 }
--- logger.go
+++ logger.go
@@ -4,36 +4,85 @@ import (
 	"log"
 )
 
+/*
+Logger represents an instance of a log structure
+*/
 type Logger struct {
-	ApplicationName string `json:"applicationName"`
+	ApplicationName string
+	LogLevel        LogType
+
+	logLevelInt int
 }
 
+/*
+Debug writes a debug entry to the log
+*/
+func (logger *Logger) Debug(message ...interface{}) {
+	logger.writeLog(DEBUG, message)
+}
+
+/*
+Error writes an error message to the log
+*/
+func (logger *Logger) Error(message ...interface{}) {
+	logger.writeLog(ERROR, message)
+}
+
+/*
+Fatal writes a fatal log messge
+*/
+func (logger *Logger) Fatal(message ...interface{}) {
+	logger.writeLog(FATAL, message)
+}
+
+/*
+Info writes an information message to the log
+*/
+func (logger *Logger) Info(message ...interface{}) {
+	logger.writeLog(INFO, message)
+}
+
+/*
+NewLogger creates a new Logger instance with a specific application name.
+The application name is a prefix used in all log messges.
+*/
 func NewLogger(applicationName string) *Logger {
 	return &Logger{
 		ApplicationName: applicationName,
+		LogLevel:        DEBUG,
+
+		logLevelInt: int(DEBUG),
 	}
 }
 
-func (this *Logger) writeLog(logType LogType, message ...interface{}) {
-	log.SetPrefix(this.ApplicationName + ": " + logType.String() + " - ")
+/*
+NewLoggerWithMinimumLevel creates a new Logger instance that only logs
+messages with a specified log type level or higher.
+*/
+func newLoggerWithMinimumLevel(applicationName string, logLevel LogType) *Logger {
+	return &Logger{
+		ApplicationName: applicationName,
+		LogLevel:        logLevel,
 
-	for _, item := range message {
-		log.Print(item)
+		logLevelInt: int(logLevel),
 	}
 }
 
-func (this *Logger) Info(message ...interface{}) {
-	this.writeLog(INFO, message)
+/*
+Warning writes a warning message to the log
+*/
+func (logger *Logger) Warning(message ...interface{}) {
+	logger.writeLog(WARN, message)
 }
 
-func (this *Logger) Warning(message ...interface{}) {
-	this.writeLog(WARN, message)
-}
+func (logger *Logger) writeLog(logType LogType, message ...interface{}) {
+	logLevelInt := int(logType)
 
-func (this *Logger) Error(message ...interface{}) {
-	this.writeLog(ERROR, message)
-}
+	if logLevelInt >= logger.logLevelInt {
+		log.SetPrefix(logger.ApplicationName + ": " + logType.String() + " - ")
 
-func (this *Logger) Fatal(message ...interface{}) {
-	this.writeLog(FATAL, message)
+		for _, item := range message {
+			log.Print(item)
+		}
+	}
 }
